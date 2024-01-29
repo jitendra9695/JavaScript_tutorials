e59981406439a97df1bae08148fdcc9b9485d65a
Jitendra Kumar Singh <jitendrassingh0987@gmail.com>
2024-01-29 21:04:35 +0530

    third commit

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -61,3 +61,47 @@ promiseFour.then((user)=>{
 })
 
 // .finally work as default action
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "JavaScript", password: '1234' })
+        }
+        else{
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+async function getAllUsers(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch(error){
+        console.log("E", error);
+    }
+}
+
+getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
